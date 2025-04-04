70badbb04acfa65edea4e624f058b8769436a03d
Atrono22 <trubu990@hotmail.com>
2025-04-04 20:13:57 +0000

    Deploying to gh-pages from @ Atrono22/herramientas-contables-Final@e91809e4bebcbacb35972aa8cff626ceba24d868 🚀

--- _next/static/chunks/app/layout-f3663195a73c75db.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{8819:function(e,n,t){Promise.resolve().then(t.t.bind(t,8877,23)),Promise.resolve().then(t.t.bind(t,6964,23)),Promise.resolve().then(t.t.bind(t,3247,23))},8877:function(){}},function(e){e.O(0,[462,971,864,744],function(){return e(e.s=8819)}),_N_E=e.O()}]);
